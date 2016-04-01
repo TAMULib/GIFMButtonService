a393dd16c9f56599c7d62cf09faaaebf0a712f33
jsavell <jsavell@library.tamu.edu>
2016-04-01 17:01:41 -0500

    add template keys to GetIt1WeekButton

--- src/main/java/edu/tamu/app/model/GetIt1WeekButton.java
+++ src/main/java/edu/tamu/app/model/GetIt1WeekButton.java
@@ -14,6 +14,10 @@ public final class GetIt1WeekButton extends AbstractGetItForMeButton {
 	
 	public GetIt1WeekButton() {
 		this.templateParameterKeys = new ArrayList<String>();
+		this.templateParameterKeys.add("title");
+		this.templateParameterKeys.add("author");
+		this.templateParameterKeys.add("isbn");
+		this.templateParameterKeys.add("publisher");
 		this.templateParameterKeys.add("callNumber");
 		this.templateParameterKeys.add("location");
 		setLinkText("Get it: 1 week");
@@ -23,6 +27,10 @@ public final class GetIt1WeekButton extends AbstractGetItForMeButton {
 	@Override
 	public String getLinkTemplate(Map<String,String> templateParameters) {
 		return "getitforme.library.tamu.edu/illiad/EVANSLocal/openurl.asp?Action=10&Form=30&sid="+this.getSID()+
+				"&title="+templateParameters.get("title")+
+				"&author="+templateParameters.get("author")+
+				"&isbn="+templateParameters.get("isbn")+
+				"&publisher="+templateParameters.get("publisher")+
 				"&rfe_dat="+templateParameters.get("callNumber")+":"+templateParameters.get("location");
 	}
 }
